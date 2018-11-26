cc4f70f67012374d9bec9f43b36a5a0ac0916bc7
zafiwk <wkdlose@163.com>
2018-11-26 13:24:49 +0800

    Initial Commit

---
+++ xianliaoim/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xianliaoim
+//
+//  Created by wangkang on 2018/11/26.
+//  Copyright © 2018 wangkang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xianliaoim/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xianliaoim
+//
+//  Created by wangkang on 2018/11/26.
+//  Copyright © 2018 wangkang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
